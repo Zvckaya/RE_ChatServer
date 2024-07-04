02b36c9bd8d5ab62cb9c0184fbc09e8f685e61d7
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-04 22:06:21 +0900

    connector를 이용한 접속

--- RE_ChatClient/Program.cs
+++ RE_ChatClient/Program.cs
@@ -1,10 +1,28 @@
 ﻿
+using Core;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace client
 {
+    class ClientSession : Session
+    {
+        public override void OnConnected(EndPoint endpoint)
+        {
+            Console.WriteLine (endpoint.ToString());
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+        }
+
+        public override void OnSend(ArraySegment<byte> buffer)
+        {
+
+        }
+    }
+
     class Client
     {
         static void Main(string[] args)
@@ -14,21 +32,15 @@ namespace client
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Connector connector = new Connector();
 
-            socket.Connect(endPoint);
+            connector.Connect(endPoint, () => { return new ClientSession(); });
 
-            byte[] sendBuff = Encoding.UTF8.GetBytes("Hello, World!");
-            socket.Send(sendBuff);
+            while (true)
+            {
 
-            byte[] recvBuff = new byte[1024];
-            int bytesReceived = socket.Receive(recvBuff);
-            string recvData = Encoding.UTF8.GetString(recvBuff, 0, bytesReceived);
-
-            Console.WriteLine($"[From Server] {recvData}");
-
-            socket.Shutdown(SocketShutdown.Both);
-            socket.Close();
+            }
+            
 
 
         }
---
+++ RE_ChatServerCore/Connector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Connector
+    {
+
+        Func<Session> _sessionFac;
+
+
+        public void Connect(IPEndPoint endPoint , Func<Session> sessionFactorty)
+        {
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _sessionFac = sessionFactorty;
+
+            SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+            e.Completed += new EventHandler<SocketAsyncEventArgs>(OnConnectedCompleted);
+            e.RemoteEndPoint = endPoint;
+            e.UserToken = socket;
+
+            RegisterConnect(e,socket);
+
+        }
+
+        private void RegisterConnect(SocketAsyncEventArgs e,Socket s)
+        {
+            if (s == null)
+                return;
+
+            bool pending = s.ConnectAsync(e);
+            if (!pending)
+                OnConnectedCompleted(null, e);
+        }
+
+        private void OnConnectedCompleted(object? sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success)
+            {
+                Session session = _sessionFac.Invoke();
+                session.Start(e.ConnectSocket);
+                session.OnConnected(e.RemoteEndPoint);
+            }
+        }
+    }
+}
